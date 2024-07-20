13817134449083848e29b620cd3d484495f25340
NamNV57 <nam.nguyenbku00@gmail.com>
2024-07-20 18:14:34 +0700

    nam commit

--- apiRequests/auth.ts
+++ apiRequests/auth.ts
@@ -2,10 +2,13 @@ import http from "./http";
 
 export const authApiRequest = {
     register: (body: RegisterDto) => 
-        http.post<ApiReponse<LoginModelDto>>('/Authentication/Register', body),
+        http.post<ApiReponse<LoginModelDto>>('/Authentication/register', body),
 
     login: (body: AccountSignInDto) =>
         http.post<ApiReponse<LoginModelDto>>('/Authentication/login', body),
+
+    logout:(userName:string |undefined)=>
+        http.put<ApiReponse<string>>(`/Authentication/revoke/${userName}`, {}),
 
     getAllImageUrl:(code:string) =>
         http.get<ApiReponse<ImageDetailDto[]>>(`/ImageDetail/GetAllByCode/${code}?fileds=PathUrl`),
