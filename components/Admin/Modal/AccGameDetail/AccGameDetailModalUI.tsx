556669f2fdcb4127a2a5cacee2e397769de90be0
demongthan <nam.nguyenbku00@gmail.com>
2024-08-29 20:59:25 +0700

    nam commit

--- components/Admin/Modal/AccGameDetail/AccGameDetailModalUI.tsx
+++ components/Admin/Modal/AccGameDetail/AccGameDetailModalUI.tsx
@@ -174,7 +174,6 @@ const AccGameDetailModalUI = ({closeModal, idAccGameDetail, idCategory, refreshA
                             <Button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm 
                             w-8 h-8 ms-auto inline-flex justify-center items-center" onClick={(event: React.MouseEvent<HTMLButtonElement>)=>{
                                 event.preventDefault();
-                                
                                 closeModal();
                             }}>
                                 <XMarkIcon className="w-6 h-6"></XMarkIcon>
