93f8a42af73d5cde360dca080a588368aad56511
demongthan <nam.nguyenbku00@gmail.com>
2024-08-30 22:40:25 +0700

    nma c

--- components/Common/DefaultPagination.tsx
+++ components/Common/DefaultPagination.tsx
@@ -48,7 +48,7 @@ const DefaultPagination = ({hasPrevious, hasNext, totalPages, currentPage, Event
             EventClickSwitchPage(currentPage-1);
           }}
         type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex jusify-center items-center 
-        gap-x-2 text-sm rounded-lg border border-cyan-400 text-gray-800 hover:bg-cyan-400 hover:text-white focus:outline-none focus:bg-cyan-400 disabled:opacity-50 disabled:pointer-events-none" aria-label="Previous">
+        gap-x-2 text-sm rounded-lg border border-cyan-400 text-gray-800 hover:bg-cyan-400 hover:text-white focus:outline-none disabled:opacity-50 disabled:pointer-events-none" aria-label="Previous">
           <svg className="shrink-0 size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="m15 18-6-6 6-6"></path>
           </svg>
